41e6e63f3b71d8237a6ff09c2f3c5216f6f9295d
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-10-18 13:03:31 -0400

    notes added and first test run on add

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 
 <p>This is the setup for my Udemy Course: The 100 Algorithms Challenge: How to Ace the JavaScript Coding Interview.</p>
 <p>There are 100 algorithms with README instructions as well as the initial function setup with a few test cases.</p>
+
+
+### How to run tests 
+`npm run testFile filepath/filename`
--- add/add.ts
+++ add/add.ts
@@ -1,12 +1,15 @@
 export function add(param1: number, param2: number): number {
-
-}
+  return param1 + param2;
+};
 
 export function add2(...param1: number[]): number {
+  let sum = 0;
 
-}
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+  return param1.reduce((a,c) => {return a + c}, sum)
+};
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+console.log(add(1, 2));
+console.log(add(3, 2));
+
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
