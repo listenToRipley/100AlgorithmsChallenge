26f96c9a8569953c3b4964d52dfa5194c47ecded
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-10-21 12:32:09 -0400

    refactor for shortest loop and passing tests

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,10 +1,9 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
-  let largest: number = 0
-  for (let i = 0; i < inputArray.length - 1; i ++) {
+  // let largest: number = 0 can start with the first element so we have one less loop
+  let largest : number = inputArray[0] * inputArray[1]
+  for (let i = 1; i < inputArray.length - 1; i ++) {
     let temp = inputArray[i] * inputArray [i + 1]
-    if (temp > largest) {
-      largest = temp
-    };
+    temp > largest ? largest = temp : largest
   };
   return largest
 };
