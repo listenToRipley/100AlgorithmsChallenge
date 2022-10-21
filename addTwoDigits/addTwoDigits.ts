80c985ee528cbbf8be72535de03acada770b288e
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-10-21 12:23:55 -0400

    add two digits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,10 @@
 export function addTwoDigits(n: any): number {
-
+  let list: string[] = `${n}`.split('')
+  let total: number = 0; 
+  list.forEach((num) => {
+    total += parseInt(num, 10)
+  })
+  return total
 }
 
 // console.log(addTwoDigits(29));
