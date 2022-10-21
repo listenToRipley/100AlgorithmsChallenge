93a2d92a30ff8d1db6a5ebc717f842e61ff4abb2
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-10-21 12:30:31 -0400

    adj element

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,12 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
+  let largest: number = 0
+  for (let i = 0; i < inputArray.length - 1; i ++) {
+    let temp = inputArray[i] * inputArray [i + 1]
+    if (temp > largest) {
+      largest = temp
+    };
+  };
+  return largest
+};
 
-}
-
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
