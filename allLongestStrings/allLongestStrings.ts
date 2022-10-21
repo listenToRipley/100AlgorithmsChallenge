fa95ea7d3698297cffd5d8c192a392faa2ebd0b4
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-10-21 12:39:09 -0400

    not passing test on longest string

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,21 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+  let longest = inputArray[0].length
 
-}
+  const newArray = (array): string[] => {
+    array.map((item) => {
+      console.log(item)
+      let result = []
+      if (item.length > longest) {
+        longest = item.length
+        newArray(array)
+      } else {
+        result.push(item)
+      }; 
+      return result
+    });
+  };
+
+  return newArray(inputArray)
+};
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
