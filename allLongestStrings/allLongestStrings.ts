3c0fb97b9cd1914d99f5421dc3a9bbca231d3481
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-10-21 13:02:17 -0400

    completed with a recusive loop

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,16 +1,30 @@
 export function allLongestStrings(inputArray: string[]): string[] {
   let longest = inputArray[0].length
-  const result : string[] = []; 
+  const result : string[] | [] = []; 
 
-  inputArray.forEach((item : string) => {
-    item.length > longest ? longest = item.length : longest
-  }); 
+  // inputArray.forEach((item : string) => {
+  //   item.length > longest ? longest = item.length : longest
+  // }); 
 
-  inputArray.forEach((item) => {
-    if(item.length === longest) {
-      result.push(item)
-    };
-  });
+  // inputArray.forEach((item) => {
+  //   if(item.length === longest) {
+  //     result.push(item)
+  //   };
+  // });
+
+  const newArray = (array : string []) => {
+    array.map((item : string) => {
+      if (item.length > longest) {
+        longest = item.length
+        result : [] = []
+        newArray(array)
+      } else if (item.length === longest) {
+        result.push(item); 
+      }; 
+    });
+  };
+
+  newArray(inputArray); 
 
   return result; 
 
